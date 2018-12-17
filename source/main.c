52b5e3e66d709d18417f517267cea1cf96ab38af
Ben <tobrasticos@gmail.com>
2018-12-18 07:46:41 +0800

    added prompt info

--- source/main.c
+++ source/main.c
@@ -2,17 +2,23 @@
 #include "main.h"
 #include "utils.h"
 
+
 int main(int argc, char * argv[]) 
 {
 	char command[BUFFSIZE];
 	strcpy(command, "");	//ensures that command doesnt start with exit
 	
+
+	char *user = getlogin(); //better to use getpwuid(geteuid()) for safety	
+	char hostname[_SC_HOST_NAME_MAX]; gethostname(hostname, _SC_HOST_NAME_MAX);
+	char thecwd[BUFFSIZE]; getcwd(thecwd, BUFFSIZE);
+
 	//command loop
 	while (1)
 	{
 		//print prompt
-			//get USER, PCNAME, CWD, and if SU or not etc
-		printf("USER@PC CWD $: ");
+			//get if SU or not 
+		printf("%s@%s %s $: ", user, hostname, thecwd);
 
 		//get input
 		getCommand(command);
